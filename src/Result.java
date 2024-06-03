93bfcf00ce267d0c1976fd2b7545fc06456e942e
Alan Teesdale <78932086+TEESDAAL@users.noreply.github.com>
2024-06-03 17:23:42 +1200

    Initial commit

---
+++ src/Result.java
@@ -0,0 +1,238 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public final class Result<T> {
+    public final static record EMPTY() {}
+    /**
+     * If non-null, the value; if null, indicates no value is present
+     */
+    private final T ok;
+    Supplier<? extends Throwable> error;
+
+
+    /**
+     * @param ok The okay value of the function.
+     */
+    private Result(T ok) {
+        Objects.requireNonNull(ok);
+        this.ok = ok;
+        this.error = null;
+    }
+
+    /**
+     * @param error A supplier of the error that this encompasses.
+     */
+    private Result(Supplier<? extends Throwable> error) {
+        Objects.requireNonNull(error);
+        this.ok = null;
+        this.error = error;
+    }
+
+    /**
+     * @param ok The value the result should hold
+     * @param <T> The type of the value
+     * @return the ok result with a given value
+     */
+    public static <T> Result<T> of(T ok) {return new Result<>(Objects.requireNonNull(ok));
+    }
+
+    /**
+     * @param error A supplier of an error - the invalid result
+     * @param <T> The type of the result
+     * @return The error result with a given supplier
+     */
+    public static <T> Result<T> of(Supplier<? extends Throwable> error) {
+        return new Result<>(Objects.requireNonNull(error));
+    }
+
+    /**
+     * @param function
+     * @param <T>
+     * @return Convert the result of a function that could throw into a result.
+     *         Which contains either the value or the error
+     */
+    public static <T> Result<T> fromFunction(Supplier<T> function) {
+        Objects.requireNonNull(function);
+        try {
+            return new Result<>(function.get());
+        } catch (Throwable e) {
+            try {
+                Constructor<? extends Throwable> errorConstructor = RuntimeException.class.getConstructor(Throwable.class);
+                Throwable initialisedError =  errorConstructor.newInstance(e);
+                return new Result<>(() -> initialisedError);
+            } catch (NoSuchMethodException | InvocationTargetException | InstantiationException |
+                     IllegalAccessException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+    }
+
+    /**
+     * @param runnable
+     * @return Convert the result of the runnable to the empty result, or an error if one occurred.
+     */
+    public static Result<EMPTY> fromFunction(Runnable runnable) {
+        return fromFunction(()-> {
+            runnable.run();
+            return new EMPTY();
+        });
+    }
+
+
+    /**
+     * @return The value if it exists, or
+     * @throws RuntimeException if the value doesn't exist.
+     */
+    public T get() throws RuntimeException {
+        if (ok == null) {
+            throw new RuntimeException(error.get());
+        }
+        return ok;
+    }
+
+    /**
+     * @return The error produced by the supplier,
+     * throws a null pointer exception if the result isOk
+     */
+    public Throwable getError() throws NullPointerException {
+        Objects.requireNonNull(error, "Result is not the error variant");
+        return error.get();
+    }
+
+
+    /**
+     * @return true if the result is okay, that is if it is not the err variant.
+     */
+    public boolean isOk() {
+        assert !Objects.equals(ok, error);
+        return ok != null;
+    }
+
+
+    /**
+     * @return true if the result is **not** okay, that is if it is the err variant.
+     */
+    public boolean hasError() {
+        assert !Objects.equals(ok, error);
+        return error!=null;
+    }
+
+    /**
+     * Run a consumer on the value if the result is okay
+     * @param action
+     */
+    public void ifOk(Consumer<? super T> action) {
+        if (isOk()) {
+            action.accept(ok);
+        }
+    }
+
+    /**
+     * Run a consumer on the value if the result is okay, otherwise run the empty action.
+     * @param action
+     * @param emptyAction
+     */
+    public void ifOkOrElse(Consumer<? super T> action, Runnable emptyAction) {
+        if (isOk()) {
+            action.accept(ok);
+        } else {
+            emptyAction.run();
+        }
+    }
+
+
+    /**
+     * Use the mapper to map the value to a different value.
+     * Keeps the error if the result is of error type.
+     * @param mapper
+     * @return
+     * @param <U>
+     */
+    public <U> Result<U> map(Function<? super T, ? extends U> mapper) {
+        Objects.requireNonNull(mapper);
+        if (hasError()) {
+            return new Result<>(error);
+        } else {
+            return new Result<>(Objects.requireNonNull(mapper.apply(ok)));
+        }
+    }
+
+    /**
+     * Works similarly to map, but flattens the result down, keeps the current error if one exists.
+     */
+    public <U> Result<U> flatMap(Function<? super T, ? extends Result<? extends U>> mapper) {
+        Objects.requireNonNull(mapper);
+        if (hasError()) {
+            return new Result<>(error);
+        }
+        Result<U> r = (Result<U>) mapper.apply(ok);
+        return Objects.requireNonNull(r);
+    }
+
+
+    public Result<T> or(Supplier<? extends Result<? extends T>> supplier) {
+        Objects.requireNonNull(supplier);
+        if (isOk()) {
+            return this;
+        }
+        @SuppressWarnings("unchecked")
+        Result<T> r = (Result<T>) supplier.get();
+        return Objects.requireNonNull(r);
+    }
+
+    public Optional<T> toOption() {
+        return Optional.ofNullable(ok);
+    }
+
+
+    public T orElse(T other) {
+        if (isOk()) {return ok;}
+        return other;
+    }
+
+
+    public T orElseGet(Supplier<? extends T> supplier) {
+        if (isOk()) {return ok;}
+        return supplier.get();
+    }
+
+
+    public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X {
+        if (isOk()) {
+            return ok;
+        }
+        throw exceptionSupplier.get();
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        return obj instanceof Result<?> other
+                && Objects.equals(ok, other.ok)
+                && Objects.equals(error, other.error);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ok, error);
+    }
+
+    @Override
+    public String toString() {
+        if (isOk()) {
+            return "Result["+ok+"]";
+        }
+        return "Result["+getError()+"]";
+    }
+}
---
+++ src/Tests.java
@@ -0,0 +1,151 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Tests {
+
+    String randomString() {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    @Test
+    void constructorTest() {
+        Random rand = new Random();
+        for (int i=0; i<100; i++) {
+            Integer x = rand.nextInt();
+            assertEquals(Result.of(x), Result.of(x));
+            assertEquals(Result.of(x).get(), x);
+        }
+
+        for (int i=0; i<100; i++) {
+            String s = randomString();
+            assertEquals(Result.of(()-> new IllegalArgumentException(s)).toString(),
+                    Result.of(()-> new IllegalArgumentException(s)).toString());
+            assertThrows(RuntimeException.class,
+                    ()->Result.of(()-> new IllegalArgumentException(s)).get(), s);
+        }
+    }
+
+    @Test
+    void fromFunctionTest() {
+       for (int i=0; i<100; i++) {
+           String s = randomString();
+           Result<Integer> r1 = Result.fromFunction(() -> throwsAssertionError(s));
+           Result<Result.EMPTY> r2 = Result.fromFunction(() -> voidThrowsAssertionError(s));
+
+           assertThrows(RuntimeException.class, ()->r1.get(), s);
+           assertThrows(RuntimeException.class, ()->r2.get(), s);
+
+           assertEquals(Result.fromFunction(()->returnsString(s)).get(), s);
+       }
+    }
+
+    @Test
+    void getterAndChecksTests() {
+        for (int i=0; i<100; i++) {
+            String s = randomString();
+            assert Result.of(s).isOk();
+            assert !Result.of(s).hasError();
+            Result.of(s).get();
+        }
+        for (int i=0; i<100; i++) {
+            String s = randomString();
+            Result<String> r = Result.of(()-> new IllegalArgumentException(s));
+            assert !r.isOk();
+            assert r.hasError();
+            assertThrows(RuntimeException.class, ()->r.get(), s);
+        }
+    }
+
+    @Test
+    void ifTests() {
+        for (int i=0; i<100; i++) {
+            String s = randomString();
+            AtomicReference<Boolean> x = new AtomicReference<>(false);
+            Result.of(s).ifOk(res -> x.set(true));
+            assert x.get();
+            Result.of(s).ifOkOrElse(
+                    res -> {assert true;},
+                    ()->{assert false;}
+            );
+        }
+        for (int i=0; i<100; i++) {
+            String s = randomString();
+            Result<String> r = Result.of(()-> new IllegalArgumentException(s));
+            AtomicReference<Boolean> x = new AtomicReference<>(true);
+            r.ifOk(res -> x.set(false));
+            assert x.get();
+            r.ifOkOrElse(
+                    res -> {assert false;},
+                    ()->{assert true;}
+            );
+        }
+    }
+
+    @Test
+    void mapTest() {
+        Random rand = new Random();
+        for (int i = 0; i < 100; i++) {
+            String s = randomString();
+            Integer x = rand.nextInt();
+
+            Result<String> r = Result.of(s);
+            assertEquals(r.map(v -> v), r);
+            assert r.map(v -> true).get();
+
+            assertEquals(
+                    Result.of(x).map(v->3*v+1),
+                    Result.of(x).flatMap(v-> Result.of(3*v+1))
+            );
+        }
+
+        for (int i = 0; i < 100; i++) {
+            String s = randomString();
+
+            Result<String> r = Result.of(() -> new IllegalArgumentException(s));
+            assert r.map(v -> 0).hasError();
+            assert r.flatMap(v -> Result.of(0)).hasError();
+        }
+    }
+
+    @Test
+    void optionTest() {
+        for (int i = 0; i < 100; i++) {
+            String s = randomString();
+
+            Result<String> r = Result.of(s);
+            assertEquals(r.toOption(), Optional.of(s));
+
+            Result<String> err = Result.of(() -> new IllegalArgumentException(s));
+            assertEquals(err.toOption(), Optional.empty());
+
+        }
+    }
+
+    Integer throwsAssertionError(String s) {
+        assert false: s;
+        return -1;
+    }
+
+    void voidThrowsAssertionError(String s) {
+        assert false: s;
+    }
+
+    String returnsString(String s) {
+        return s;
+    }
+}
